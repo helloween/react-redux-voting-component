ef479783d17270ff350dce1c95e8bba2bf33d1c8
Yury Gurski <hi1sailor@gmail.com>
2015-12-09 19:17:42 +0300

    added vote, winner components

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Vote from './Vote';
+import Winner from './Winner';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
+
   getPair: function() {
     return this.props.pair || [];
   },
+
+  isDisabled: function () {
+    return !!this.props.hasVoted;
+  },
+
+  hasVotedFor: function (entry) {
+    return this.props.hasVoted === entry;
+  },
+
   render: function() {
-    return <div className="voting">
-      {this.getPair().map(entry =>
-        <button key={entry}>
-          <h1>{entry}</h1>
-        </button>
-      )}
+    return <div>
+      {this.props.winner ? <Winner ref="winner"/> : <Vote pair="this.props.pair" hasVoted="this.props.hasVoted"/>}
     </div>;
   }
 });
